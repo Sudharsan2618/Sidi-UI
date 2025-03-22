0e73d9a25740adab6f1ba120663cb7ac9f2251b1
Sudharsan2618 <105963347+Sudharsan2618@users.noreply.github.com>
2025-03-22 17:40:24 +0530

    api changed

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lms-be-sqpa.onrender.com/api",
+  baseURL: "https://sidi-be.onrender.com/api",
 });
 
 export default api;
