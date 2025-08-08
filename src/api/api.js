fe04baac9112d2c417b75b75850851fd19150e3a
Sudharsan D S <105963347+Sudharsan2618@users.noreply.github.com>
2025-08-08 11:10:12 +0530

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://sidi-be.onrender.com/api',
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://sidi-be.onrender.com/api',
+    // baseURL: 'http://localhost:5000/api',
     headers: {
         'Content-Type': 'application/json',
     },
